80a1cb3a098ec9adb9373c698691367bf62ace82
YoungLeeNENU <youngleemails@gmail.com>
2016-12-31 14:55:22 +0800

    add theme

--- src/clj/ylpn_fe_main/css.clj
+++ src/clj/ylpn_fe_main/css.clj
@@ -2,5 +2,8 @@
   (:require [garden.def :refer [defstyles]]))
 
 (defstyles screen
-  [:body {:color "red"}]
-  [:.level1 {:color "green"}])
+  [:body {:color "red"
+          :background-color "#333333"}]
+  [:.level1 {:color "#d7d7d7"}]
+  [:.ylpn-links {:color "#9f79ee"
+                 :text-decoration "none"}])
